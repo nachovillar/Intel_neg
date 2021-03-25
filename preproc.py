5967a2cbcfa8057aeaf2a63dc57d888a48a211bd
José Ignacio Villar <jose.villar.12@sansano.usm.cl>
2021-03-25 20:05:45 -0300

    Primeros paso
    Se empieza a crear el preproc.py para dejar los datos listos

---
+++ preproc.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+    
+    X = pd.read_csv('x_input.csv', sep=",", header=None)
