e1db044503b3154ff40d05c10e1e9b5a30186478
José Ignacio Villar <jose.villar.12@sansano.usm.cl>
2021-03-28 17:05:03 -0300

    train.py listo
    se completa el train.py al dejar lista la función save_w_npy() en utilities y también la load_w_npy()

--- my_utility.py
+++ my_utility.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
 import math
-import pickle  #importamos uno mas
-
-
 
 def load_data_txt(inp, out):
     xe = pd.read_csv(inp, sep=",", header=None)
@@ -18,22 +15,11 @@ def iniW(hn, n0):
     return w
 
 def save_w_npy(w1, w2):
-    # HACER FUNCION QUE GUARDA AMBAS MATRICES EN UN ARCHIVO
-    #escribir el resutado
-    #guardar w1,w2 en un csv se pueden juntar las 2 matrices para facilitar la operacion
-    #para machine learning hoy en dia se ocupan los pickles para guardar arrays
-    pesos = open("pesos.pickle", "wb")
-    pickle.dump(w1, pesos)
-    pickle.dump(w2, pesos)
-    pesos.close()
+    np.savez("pesos", w1=w1, w2=w2)
     
-    #pd.read_csv(inp, sep=",", header=None)
-    
-
 def load_w_npy(filename):
-    #pesos_load = pickle.load(open("pesos.pickle", "rb"))
-    pesos = open("pesos.pickle")
-    w1= pickle.load("pesos.pickle")
-    w2= pickle.load("pesos.pickle")
-    pesos.close
+    container = np.load(filename)
+    weight_data = [container[key] for key in container]
+    w1 = weight_data[0]
+    w2 = weight_data[1]
     return(w1, w2)
