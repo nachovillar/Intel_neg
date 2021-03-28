d5caac3ea60d625938d2cdacbcf88c339eb2cf3c
Pedro G <pedrohirobuy@gmail.com>
2021-03-27 22:43:53 -0300

    Usamos Pickle para guardar w1 y w2
    el profe Hector recomienda esto https://programacion.net/articulo/los_pickles_de_python_1860

--- my_utility.py
+++ my_utility.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import math
-
+import pickle  #importamos uno mas
 
 
 
@@ -19,8 +19,21 @@ def iniW(hn, n0):
 
 def save_w_npy(w1, w2):
     # HACER FUNCION QUE GUARDA AMBAS MATRICES EN UN ARCHIVO
-    return
+    #escribir el resutado
+    #guardar w1,w2 en un csv se pueden juntar las 2 matrices para facilitar la operacion
+    #para machine learning hoy en dia se ocupan los pickles para guardar arrays
+    pesos = open("pesos.pickle", "wb")
+    pickle.dump(w1, pesos)
+    pickle.dump(w2, pesos)
+    pesos.close()
+    
+    #pd.read_csv(inp, sep=",", header=None)
+    
 
 def load_w_npy(filename):
-    
-    print(w1, w2)
+    #pesos_load = pickle.load(open("pesos.pickle", "rb"))
+    pesos = open("pesos.pickle")
+    w1= pickle.load("pesos.pickle")
+    w2= pickle.load("pesos.pickle")
+    pesos.close
+    return(w1, w2)
--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import my_utility as ut
 def main():
     inp = "test_x.csv"
     out = "test_y.csv"
-    file_w = 'pesos.npz'
+    file_w = 'pesos.pickle'
     xv, yv = ut.load_data_txt(inp, out)
     w1, w2 = ut.load_w_npy(file_w)
     zv = ut.snn_ff(xv,w1,w2)
